188d1611313ac9b1e88e759ff8918ed6eae48fbf
tikimeat <>
2018-01-19 20:21:47 +0000

    Implemented the buy button although it does not currenctly work due to a limitation in truffle/testrpc

--- frontend/app.js
+++ frontend/app.js
@@ -20,6 +20,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use("/ws", express.static(path.join(__dirname, '../build/contracts')));
+console.log(path.join(__dirname, '../build/contracts'));
 
 app.use('/', index);
 
--- frontend/public/js/lottery.js
+++ frontend/public/js/lottery.js
@@ -3,7 +3,7 @@
 /**
  * Instance variables needed to interact with deployed contract
  */
-var contract_abidefinition = [ { "constant": true, "inputs": [], "name": "rate", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "endTime", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "cap", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "goal", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "weiRaised", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [], "name": "finalize", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "wallet", "outputs": [ { "name": "", "type": "address" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "startTime", "outputs": [ { "name": "", "type": "uint256" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "goalReached", "outputs": [ { "name": "", "type": "bool" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "isFinalized", "outputs": [ { "name": "", "type": "bool" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "owner", "outputs": [ { "name": "", "type": "address" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [], "name": "claimRefund", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": false, "inputs": [ { "name": "beneficiary", "type": "address" } ], "name": "buyTokens", "outputs": [], "payable": true, "stateMutability": "payable", "type": "function" }, { "constant": true, "inputs": [], "name": "hasEnded", "outputs": [ { "name": "", "type": "bool" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": false, "inputs": [ { "name": "newOwner", "type": "address" } ], "name": "transferOwnership", "outputs": [], "payable": false, "stateMutability": "nonpayable", "type": "function" }, { "constant": true, "inputs": [], "name": "vault", "outputs": [ { "name": "", "type": "address" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "constant": true, "inputs": [], "name": "token", "outputs": [ { "name": "", "type": "address" } ], "payable": false, "stateMutability": "view", "type": "function" }, { "inputs": [ { "name": "_startTime", "type": "uint256" }, { "name": "_endTime", "type": "uint256" }, { "name": "_rate", "type": "uint256" }, { "name": "_goal", "type": "uint256" }, { "name": "_cap", "type": "uint256" }, { "name": "_wallet", "type": "address" } ], "payable": false, "stateMutability": "nonpayable", "type": "constructor" }, { "payable": true, "stateMutability": "payable", "type": "fallback" }, { "anonymous": false, "inputs": [], "name": "Finalized", "type": "event" }, { "anonymous": false, "inputs": [ { "indexed": true, "name": "previousOwner", "type": "address" }, { "indexed": true, "name": "newOwner", "type": "address" } ], "name": "OwnershipTransferred", "type": "event" }, { "anonymous": false, "inputs": [ { "indexed": true, "name": "purchaser", "type": "address" }, { "indexed": true, "name": "beneficiary", "type": "address" }, { "indexed": false, "name": "value", "type": "uint256" }, { "indexed": false, "name": "amount", "type": "uint256" } ], "name": "TokenPurchase", "type": "event" } ];
+var contract_abidefinition = ""
 var contract_address = "0x345ca3e014aaf5dca488057592ee47305d9b3e10"
 var contract;
 
@@ -23,8 +23,15 @@ function startApp() {
     if (web3 && web3.isConnected()) {
         console.log("Web 3 is connected");
     }
+    
+    getAbiDefinition(retrieveDeployedContract)
+}
 
-    retrieveDeployedContract();
+function getAbiDefinition(callback) {
+    $.getJSON("/ws/Lottery.json", function(def) {
+        contract_abidefinition = def["abi"];
+        callback();
+    });
 }
 
 function retrieveDeployedContract() {
@@ -54,6 +61,17 @@ function updateUI(docElementId, html)  {
 
 $(function() {
     $('#btn-buy').click(function() {
-        alert('Smart contract integration code gose here');
+        web3.eth.getAccounts(function(error, accounts) {
+            if (error) {
+                console.log(error);
+            }
+
+            var account = accounts[0];
+            var numTokens = parseInt($("#num-tickets").val());
+            
+            contract.buyTokens.sendTransaction(account, { from: account, value: numTokens * 1000 })
+            
+            //web3.eth.sendTransaction({to:contract_address, from:account, value: 1000});
+        });
     });
 })
