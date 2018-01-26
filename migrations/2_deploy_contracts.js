763f61668abaa0e2af830423ff8aa59247eeed22
Bharat Kunwar <b.kunwar@gmail.com>
2018-01-26 10:31:55 +0000

    Changed lottery end time for 7 days to 24 hours

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -2,7 +2,7 @@ var Lottery = artifacts.require("./Lottery.sol");
 
 module.exports = function(deployer) {
   var startTime = web3.eth.getBlock('latest').timestamp
-  var endTime = startTime + (86400 * 7) // 1 week
+  var endTime = startTime + (24*60*60) // 1 day
   const rate = new web3.BigNumber(1000)
   const wallet = web3.eth.accounts[1]
   deployer.deploy(Lottery, startTime, endTime, rate, wallet);
