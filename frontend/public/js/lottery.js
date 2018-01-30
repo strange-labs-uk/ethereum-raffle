d4c8ecbbcc45edb148cbe20315a51a5fe34cbc09
Bharat Kunwar <b.kunwar@gmail.com>
2018-01-30 11:12:09 +0000

    Sexy front-end with particle-js@

--- frontend/public/js/lottery.js
+++ frontend/public/js/lottery.js
@@ -262,4 +262,61 @@ function initializeClock(untilTime) {
     var timeInterval = setInterval(updateClock, 1000);
 }
 
-// ---- END OF COUNTDOWN
+// ---- PARTICLE
+$.getScript('http://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js', function() {
+    particlesJS("particles-js", {
+      particles: {
+        number: { value: 50, density: { enable: true, value_area: 800 } },
+        color: { value: "#ffffff" },
+        shape: {
+          type: "circle",
+          stroke: { width: 0, color: "#000000" },
+          polygon: { nb_sides: 5 },
+          image: { src: "img/github.svg", width: 100, height: 100 }
+        },
+        opacity: {
+          value: 0.6,
+          random: true,
+          anim: { enable: true, speed: 1, opacity_min: 0.2, sync: false }
+        },
+        size: {
+          value: 12.02559045649142,
+          random: true,
+          anim: { enable: false, speed: 40, size_min: 0.1, sync: false }
+        },
+        line_linked: {
+          enable: true,
+          distance: 200,
+          color: "#ffffff",
+          opacity: 0.5,
+          width: 1
+        },
+        move: {
+          enable: true,
+          speed: 10,
+          direction: "none",
+          random: true,
+          straight: false,
+          out_mode: "out",
+          bounce: false,
+          attract: { enable: false, rotateX: 600, rotateY: 1200 }
+        }
+      },
+      interactivity: {
+        detect_on: "canvas",
+        events: {
+          onhover: { enable: true, mode: "grab" },
+          onclick: { enable: false, mode: "repulse" },
+          resize: true
+        },
+        modes: {
+          grab: { distance: 400, line_linked: { opacity: 1 } },
+          bubble: { distance: 400, size: 40, duration: 2, opacity: 8, speed: 3 },
+          repulse: { distance: 200, duration: 0.4 },
+          push: { particles_nb: 4 },
+          remove: { particles_nb: 2 }
+        }
+      },
+      retina_detect: true
+    });
+});
