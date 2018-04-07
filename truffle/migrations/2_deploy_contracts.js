7fdf434863a4921d69fafc3b88abda8e6355c707
Bharat Kunwar <b.kunwar@gmail.com>
2018-04-07 01:10:53 +0100

    Create the first game automatically when the contract is first deployed
    Closes #22

--- truffle/migrations/2_deploy_contracts.js
+++ truffle/migrations/2_deploy_contracts.js
@@ -3,3 +3,19 @@ var HashKeyRaffle = artifacts.require("./HashKeyRaffle.sol");
 module.exports = function(deployer) {
   deployer.deploy(HashKeyRaffle);
 };
+
+var price = 1000;
+var secretKeyHash = 'apple';
+var drawPeriod = 30; // seconds
+var start = web3.eth.getBlock('latest').timestamp + 60; //seconds
+var end = start + (5 * 60); // seconds
+var feePercent = 10;
+var minPlayers = 1;
+var account = web3.eth.accounts[0];
+
+// Create a new game using the deployed contract
+HashKeyRaffle.deployed().then(function(instance) {
+  return instance.newGame(price, secretKeyHash, drawPeriod, start, end, feePercent, minPlayers, {from: account});
+}).then(function(result){
+  console.log('HashKeyRaffle.currentGameIndex:', web3.toDecimal(result.logs[0].args.gameIndex));
+}).catch(error => console.log(error));
